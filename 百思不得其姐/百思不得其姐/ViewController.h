0f167df16519fa14d7c7afc7e86cba582426e8a4
冯庄玥 <fengzhuangyue@promote.cache-dns.local>
2016-02-28 21:42:09 +0800

    初始化项目

---
+++ 百思不得其姐/百思不得其姐/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  百思不得其姐
+//
+//  Created by 冯庄玥 on 16/2/28.
+//  Copyright © 2016年 冯庄玥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 百思不得其姐/百思不得其姐/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  百思不得其姐
+//
+//  Created by 冯庄玥 on 16/2/28.
+//  Copyright © 2016年 冯庄玥. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
