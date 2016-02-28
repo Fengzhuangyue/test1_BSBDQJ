4f9350858fb5ed6f5434ed06e9ae46164cb68a91
冯庄玥 <fengzhuangyue@promote.cache-dns.local>
2016-02-28 22:58:04 +0800

    初始化项目

--- 百思不得其姐/百思不得其姐/ViewController.m
+++ 百思不得其姐/百思不得其姐/ViewController.m
@@ -17,11 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor=[UIColor blueColor];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
 }
 
 @end
